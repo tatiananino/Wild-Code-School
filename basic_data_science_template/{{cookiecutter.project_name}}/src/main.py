d4d05c31c31cc2acf884eedb68446245dbedcacf
tatiananino <tatianac.nino@gmail.com>
2025-01-16 20:51:37 +0100

    Add fichier main

---
+++ basic_data_science_template/{{cookiecutter.project_name}}/src/main.py
@@ -0,0 +1,13 @@
+# main.py
+
+"""
+Project: {{cookiecutter.project_name}}
+Author: {{cookiecutter.author_name}}
+Description: {{cookiecutter.description}}
+"""
+
+def main():
+    print("Hello, world!")
+
+if __name__ == "__main__":
+    main()
